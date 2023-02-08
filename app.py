32b51433404bef46fe5a2cab473cc5bc7ab1b049
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-08 11:02:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,12 @@ st.title("Operating Expense Summary by Squad")
 # load static file for now
 df = pd.read_csv('Aragon_financial_simple.csv')
 
-palette_color = sns.color_palette('pastel')
-
 # bar chart - MoM Expense by Squad
 fig = plt.figure(figsize=(10, 4))
 g = sns.catplot(
     data=df, x="Month", y="Amount", hue="Squad", kind="bar",
     sharex=False, margin_titles=True,
-    aspect=4,ci=None, colors=palette_color
+    aspect=4,ci=None
 )
 g.set(xlabel="Month", ylabel="Amount", title='MoM Expense by Squad')
 
@@ -28,24 +26,10 @@ df_=df[['Squad', 'Amount']].copy()
 data=df_.groupby(['Squad']).sum()['Amount']
 labels = df_['Squad'].unique().tolist()
 fig1, ax1 = plt.subplots()
-
+palette_color = sns.color_palette('pastel')
 ax1.pie(data, labels=labels, autopct='%1.1f%%',startangle=90, colors=palette_color)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 st.pyplot(fig1)
   
-# # define Seaborn color palette to use
-# palette_color = sns.color_palette('pastel')
-  
-# # plotting data on chart
-# pie = plt.pie(data, labels=labels, colors=palette_color, autopct='%.0f%%')
-
-# pie = plt.title("Expense Breakdown by Squad")
-         
-# # displaying chart
-# plt.show()
-
-# st.pyplot(pie)
-    
-
 st.write(df)
