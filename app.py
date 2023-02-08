5cafd8769ee045774f36ea29e1f24b68a2364d44
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-08 10:39:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,16 @@ import matplotlib.pyplot as plt
 st.title("A Simple Streamlit Web App")
 name = st.text_input("Enter your name", '')
 st.write(f"Hello {name}!")
-x = st.slider("Select an integer x", 0, 10, 1)
-y = st.slider("Select an integer y", 0, 10, 1)
-df = pd.DataFrame({"x": [x], "y": [y] , "x + y": [x + y]}, index = ["addition row"])
+# x = st.slider("Select an integer x", 0, 10, 1)
+# y = st.slider("Select an integer y", 0, 10, 1)
+df = pd.read_csv('Aragon financial simple.csv')
+
+with sns.axes_style('white'):
+    g = sns.catplot(
+        data=df, x="Month", y="Amount", hue="Squad", kind="bar",
+        sharex=False, margin_titles=True,
+        aspect=4,ci=None
+    )
+    g.set(xlabel="Month", ylabel="Amount", title='MoM Expense by Squad')
+    
 st.write(df)
