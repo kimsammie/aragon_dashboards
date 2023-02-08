fdb7b2ce7725946c461a6d0ee6dacff10def2ab3
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-08 12:06:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,27 +9,29 @@ st.title("Operating Expense Summary by Squad")
 # load static file for now
 df = pd.read_csv('Aragon_financial_simple.csv')
 
+g1, g2 = st.columns((1,1))
+
 # bar chart - MoM Expense by Squad
 fig = plt.figure(figsize=(10, 4))
-g = sns.catplot(
+g1 = sns.catplot(
     data=df, x="Month", y="Amount", hue="Squad", kind="bar",
     sharex=False, margin_titles=True,
     aspect=4,errorbar=None
 )
-g.set(xlabel="Month", ylabel="Amount", title='MoM Expense by Squad')
+g1.set(xlabel="Month", ylabel="Amount", title='MoM Expense by Squad')
 
-st.pyplot(g)
+st.pyplot(g1)
 
 
 # copy data for the pie chart    
 df_=df[['Squad', 'Amount']].copy()
 data=df_.groupby(['Squad']).sum()['Amount']
 labels = df_['Squad'].unique().tolist()
-fig1, ax1 = plt.subplots()
+g2, ax1 = plt.subplots()
 palette_color = sns.color_palette('pastel')
 ax1.pie(data, labels=labels, autopct='%1.1f%%',startangle=90, colors=palette_color)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-st.pyplot(fig1)
+st.pyplot(g2)
   
 st.write(df)
