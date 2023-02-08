fde33a8b0ce56905fdedaa6fa2c7ceab5f13f43e
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-08 11:00:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,12 @@ import seaborn as sns
 sns.set()
 import matplotlib.pyplot as plt
 
-st.title("A Simple Streamlit Web App")
-name = st.text_input("Enter your name", '')
-st.write(f"Hello {name}!")
-# x = st.slider("Select an integer x", 0, 10, 1)
-# y = st.slider("Select an integer y", 0, 10, 1)
+st.title("Operating Expense Summary by Squad")
+
+# load static file for now
 df = pd.read_csv('Aragon_financial_simple.csv')
 
-
+# bar chart - MoM Expense by Squad
 fig = plt.figure(figsize=(10, 4))
 g = sns.catplot(
     data=df, x="Month", y="Amount", hue="Squad", kind="bar",
@@ -20,25 +18,16 @@ g = sns.catplot(
 )
 g.set(xlabel="Month", ylabel="Amount", title='MoM Expense by Squad')
 
-# titanic = sns.load_dataset("titanic")
-
-# fig = plt.figure(figsize=(10, 4))
-# sns.countplot(x="class", data=titanic)
-
 st.pyplot(g)
-    
+
+
+# copy data for the pie chart    
 df_=df[['Squad', 'Amount']].copy()
-# data=df_['Amount']
 data=df_.groupby(['Squad']).sum()['Amount']
-
 labels = df_['Squad'].unique().tolist()
-
-# labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-# sizes = [15, 30, 45, 10]
-# explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-
 fig1, ax1 = plt.subplots()
-ax1.pie(data, labels=labels, autopct='%1.1f%%',startangle=90)
+palette_color = sns.color_palette('pastel')
+ax1.pie(data, labels=labels, autopct='%1.1f%%',startangle=90, colors=palette_color)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 st.pyplot(fig1)
