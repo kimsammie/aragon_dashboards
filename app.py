9463c83911e78539c42db5bcda7eef5ad907f66a
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-08 10:36:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,26 +4,10 @@ import seaborn as sns
 sns.set()
 import matplotlib.pyplot as plt
 
-def write():
-    st.title("Aragon Discord Channel Topics Discussed by the Community ")
-
-    st.markdown(
-        """
-	<br><br/>
-	
-	Using topic modeling, we can extract the "hidden topics" from large volumes of messages in Aragon Discord channels. 
-	
-	Please see the most popular topics discussed for a given time period by first selecting the start date and end date for a channel of your interest.	
-	
-	After that, try different number of topics (e.g., a higher number for a longer time period) until you see coherent topics (i.e., words in the topic support each other). 
-	
-	At the bottom, please also check out the overall sentiment found in the messages for the chosen time period. 
-	
-	""",
-        unsafe_allow_html=True,
-    )
-    df = pd.read_csv('Aragon financial simple.csv')
-    st.write("Number of Topics:", df)
-    st.sidebar.write(
-        "[Source Code](https://github.com/kimsammie/Aragon_Discord_Metrics)"
-    )
+st.title(“A Simple Streamlit Web App”)
+name = st.text_input(“Enter your name”, ‘’)
+st.write(f”Hello {name}!”)
+x = st.slider(“Select an integer x”, 0, 10, 1)
+y = st.slider(“Select an integer y”, 0, 10, 1)
+df = pd.DataFrame({“x”: [x], “y”: [y] , “x + y”: [x + y]}, index = [“addition row”])
+st.write(df)
