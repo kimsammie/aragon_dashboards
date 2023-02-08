03beadf9c6bcc6fce68a3048611d335e34920f6a
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-08 10:47:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,21 +11,21 @@ st.write(f"Hello {name}!")
 # y = st.slider("Select an integer y", 0, 10, 1)
 df = pd.read_csv('Aragon_financial_simple.csv')
 
-with sns.axes_style('white'):
-    fig = plt.figure(figsize=(10, 4))
-    g = sns.catplot(
-        data=df, x="Month", y="Amount", hue="Squad", kind="bar",
-        sharex=False, margin_titles=True,
-        aspect=4,ci=None
-    )
-    g.set(xlabel="Month", ylabel="Amount", title='MoM Expense by Squad')
+
+fig = plt.figure(figsize=(10, 4))
+g = sns.catplot(
+    data=df, x="Month", y="Amount", hue="Squad", kind="bar",
+    sharex=False, margin_titles=True,
+    aspect=4,ci=None
+)
+g.set(xlabel="Month", ylabel="Amount", title='MoM Expense by Squad')
 
 # titanic = sns.load_dataset("titanic")
 
 # fig = plt.figure(figsize=(10, 4))
 # sns.countplot(x="class", data=titanic)
 
-    st.pyplot(fig)
+st.pyplot(g)
     
 df_=df[['Squad', 'Amount']].copy()
 # data=df_['Amount']
