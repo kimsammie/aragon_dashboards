77c83c99f28723613c7b044c896923e1289bd1df
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-08 10:38:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import seaborn as sns
 sns.set()
 import matplotlib.pyplot as plt
 
-st.title(“A Simple Streamlit Web App”)
-name = st.text_input(“Enter your name”, ‘’)
-st.write(f”Hello {name}!”)
-x = st.slider(“Select an integer x”, 0, 10, 1)
-y = st.slider(“Select an integer y”, 0, 10, 1)
-df = pd.DataFrame({“x”: [x], “y”: [y] , “x + y”: [x + y]}, index = [“addition row”])
+st.title("A Simple Streamlit Web App")
+name = st.text_input("Enter your name", '')
+st.write(f"Hello {name}!")
+x = st.slider("Select an integer x", 0, 10, 1)
+y = st.slider("Select an integer y", 0, 10, 1)
+df = pd.DataFrame({"x": [x], "y": [y] , "x + y": [x + y]}, index = ["addition row"])
 st.write(df)
