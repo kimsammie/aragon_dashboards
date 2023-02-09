9a439e31246f10ef92a44b35113ef0d397627c8e
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-09 11:45:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,41 +10,55 @@ st.set_page_config(layout="wide")
 
 st.title("Operating Expense Summary by Squad")
 
-# load static file for now
-df = pd.read_csv('Aragon_financial_simple.csv')
+tab1, tab2, tab3, tab4 = st.tabs(["OpEx", "Revenue", "Income Statement", "Balance Sheet"])
 
-col1, col2 = st.columns((1.5,1))
+with tab1:
+  st.header("A cat")
 
-with col1:
-  fig = px.histogram(df, x = 'Month', y='Amount', color="Squad", template = 'seaborn', barmode='group')
-  fig.update_layout(title_text="MoM Expense by Squad",
-                    yaxis_title="Amount", xaxis_title="Month")
-  col1.plotly_chart(fig, use_container_width=True) 
+  # load static file for now
+  df = pd.read_csv('Aragon_financial_simple.csv')
+
+  col1, col2 = st.columns((1.5,1))
+
+  with col1:
+    fig = px.histogram(df, x = 'Month', y='Amount', color="Squad", template = 'seaborn', barmode='group')
+    fig.update_layout(title_text="MoM Expense by Squad",
+                      yaxis_title="Amount", xaxis_title="Month")
+    col1.plotly_chart(fig, use_container_width=True) 
 
 
-with col2:
-  # copy data for the pie chart    
-  df_=df[['Squad', 'Amount']].copy()
-  data=df_.groupby(['Squad']).sum()['Amount']
-  labels = df_['Squad'].unique().tolist()
+  with col2:
+    # copy data for the pie chart    
+    df_=df[['Squad', 'Amount']].copy()
+    data=df_.groupby(['Squad']).sum()['Amount']
+    labels = df_['Squad'].unique().tolist()
 
-  fig = px.pie(df_, values=data, names=labels, template = 'seaborn')
+    fig = px.pie(df_, values=data, names=labels, template = 'seaborn')
 
-  fig.update_layout(title_text="Expense Breakdown by Squad")
-  col2.plotly_chart(fig, use_container_width=True)  
+    fig.update_layout(title_text="Expense Breakdown by Squad")
+    col2.plotly_chart(fig, use_container_width=True)  
+
+
+  # display raw data
+  fig = go.Figure(data=[go.Table(
+      header=dict(values=list(df.columns),
+                  fill_color='paleturquoise',
+                  align='left'),
+      cells=dict(values=[df.Squad, df.Category, df.Detail, df.Month, df.Amount],
+                 fill_color='lavender',
+                 align='left'))
+  ])
+
+  fig.update_layout(title_text="Raw Expense Data",title_font_color = '#264653',title_x=0,margin= dict(l=0,r=10,b=10,t=30), height=400)                                                               
+  st.plotly_chart(fig, use_container_width=True)      
+
+with tab2:
+   st.header("Revenue")
+
+
+with tab3:
+   st.header("Income Statement")
 
   
-# display raw data
-fig = go.Figure(data=[go.Table(
-    header=dict(values=list(df.columns),
-                fill_color='paleturquoise',
-                align='left'),
-    cells=dict(values=[df.Squad, df.Category, df.Detail, df.Month, df.Amount],
-               fill_color='lavender',
-               align='left'))
-])
-
-fig.update_layout(title_text="Raw Expense Data",title_font_color = '#264653',title_x=0,margin= dict(l=0,r=10,b=10,t=30), height=400)                                                               
-st.plotly_chart(fig, use_container_width=True)      
-
-
+with tab4:
+   st.header("Balance Sheet")
