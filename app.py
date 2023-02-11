d0478ee4f7b411cfdc9c1893e6a32a7ef73856c2
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-11 13:27:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ with tab2:
                     align='left'),
         cells=dict(values=[revenue_df.index, revenue_df.Nov_22_Projected,revenue_df.Nov_22_Actual, revenue_df.Dec_22_Projected, revenue_df.Dec_22_Actual,
             revenue_df.Jan_23_Projected,revenue_df.Jan_23_Actual,revenue_df.Feb_23_Projected,revenue_df.Feb_23_Actual],
-                   fill_color='palegreen',
+                   fill_color='mintcream',
                    align='left'))
     ])
 
