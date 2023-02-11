4968e11f0832d761aeed16e226e551292242a8eb
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-11 12:02:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ with tab1:
   # display raw data
   fig = go.Figure(data=[go.Table(
       header=dict(values=list(df.columns),
-                  fill_color='paleturquoise',
+                  fill_color='brown',
                   align='left'),
       cells=dict(values=[df.Squad, df.Category, df.Detail, df.Month, df.Amount],
                  fill_color='ivory',
