61e3aa205f14f0116859dcfe94e7015b384507f3
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-13 11:33:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -217,7 +217,7 @@ with tab3:
 
       fig.update_layout(
               title = "Key Financial Drivers",
-              showlegend = True,row=1, col=1
+              showlegend = True
       )
 
       fig.add_trace(go.Scatter(x=[20, 30, 40], y=[50, 60, 70]),
