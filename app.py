74e50424a6ba50bbbce6bc467a8f38d7faf5d878
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-13 10:04:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -194,7 +194,7 @@ with tab3:
   
   
 
-      fig = make_subplots(rows=2, cols=1)
+      fig = make_subplots(rows=2, cols=1, subplot_titles=('Subplot title1',  'Subplot title2')
 
 #       fig.add_trace(go.Scatter(x=[1, 2, 3], y=[4, 5, 6]),
 #                     row=1, col=1)
@@ -221,7 +221,6 @@ with tab3:
       )
 
       fig.add_trace(go.Scatter(x=[20, 30, 40], y=[50, 60, 70]),
-                    title = "Net Income Trend",
                     row=2, col=1)
 
 #       fig.show()
