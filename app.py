35f729c5f624223ee28f8c942d6ccd600e92a127
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-23 15:18:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -489,32 +489,18 @@ with tab7:
   col1, col2 = st.columns((1,1))
 
   with col1:
+#     st.write("TBD")
+    fig = px.histogram(df_, x = 'Date', y='Amount', color="Workspace Name", template = 'seaborn', barmode='group')
+    fig.update_layout(title_text="MoM Expense by Workspace",
+                      yaxis_title="Amount", xaxis_title="Month")
+    col1.plotly_chart(fig, use_container_width=True) 
+    
+  with col2:
+    st.write("Top 5 Contributors by Amount Paid")
     df_agg = df_.groupby(['Date','Assignee', 'Workspace Name']).agg({'Amount':sum})
     g = df_agg['Amount'].groupby('Date', group_keys=False)
     res = g.apply(lambda x: x.sort_values(ascending=False).head(5))
     st.write(res)
-#     fig = px.histogram(df, x = 'Month', y='Amount', color="Squad", template = 'seaborn', barmode='group')
-#     fig.update_layout(title_text="MoM Expense by Squad",
-#                       yaxis_title="Amount", xaxis_title="Month")
-#     col1.plotly_chart(fig, use_container_width=True) 
-
-
-  with col2:
-    st.write("TBD")
-    # copy data for the pie chart    
-#     df_=df[['Squad', 'Amount']].copy()
-#     data=df_.groupby(['Squad']).sum()['Amount']
-#     labels = df_['Squad'].unique().tolist()
-
-#     fig = px.pie(df_, values=data, names=labels, template = 'seaborn')
-
-#     fig.update_layout(title_text="Expense Breakdown by Squad")
-#     col2.plotly_chart(fig, use_container_width=True)  
-    
-    fig = px.histogram(df_, x = 'Date', y='Amount', color="Workspace Name", template = 'seaborn', barmode='group')
-    fig.update_layout(title_text="MoM Expense by Squad",
-                      yaxis_title="Amount", xaxis_title="Month")
-    col2.plotly_chart(fig, use_container_width=True) 
 
 #   # display raw data
 #   fig = go.Figure(data=[go.Table(
