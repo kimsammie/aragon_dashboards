62dc5e61896577bcdd7f5dc1e3a7fb6430101ba1
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-23 12:10:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -448,7 +448,7 @@ with tab6:
     labels = transfers_short_['squad'].unique().tolist()
 
 #     fig = px.pie(transfers_short_, values=data, names=labels, template = 'seaborn')
-    fig = px.pie(transfers_short_, values=data, template = 'seaborn')
+    fig = px.pie(transfers_short_, values=data, names="squad",template = 'seaborn')
 
     fig.update_layout(title_text="Expense Breakdown by Squad")
     col2.plotly_chart(fig, use_container_width=True)  
