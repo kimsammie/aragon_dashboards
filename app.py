551af57676e33e6a4eb5dffe55bff012e5f8915d
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-23 15:09:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ st.title("Acme Financial Reporting")
 st.write("")
 st.write("")
 
-tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["📈 OpEx", "Revenue", "Income Statement", "Balance Sheet", "Admin Input", "OpEx from Admin Input"])
+tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["📈 OpEx", "Revenue", "Income Statement", "Balance Sheet", "Admin Input", 
+                                                    "OpEx from Admin Input", "Workspace Expenses Detail"])
 
 with tab1:
   st.header("Operating Expense")
@@ -255,6 +256,7 @@ with tab4:
     
   fig = go.Figure()
 
+  # https://plotly.com/python/indicator/
   fig.add_trace(go.Indicator(
       mode = "number+delta",
       value = 44055,
@@ -471,6 +473,57 @@ with tab6:
 
   fig.update_layout(title_text="Raw Expense Data",title_font_color = '#264653',title_x=0,margin= dict(l=0,r=10,b=10,t=30), height=400)                                                               
   st.plotly_chart(fig, use_container_width=True)   
+
+with tab7:
+  st.header("Workspace Expenses Detail")
+  st.write("TBD")
+  st.write("")
+  st.write("")
+  st.write("")
+  # load static file for now
+  df = pd.read_csv('DeWork.csv',encoding='windows-1252')
+  df_=df[df['Amount'].notnull()]
+
+  col1, col2 = st.columns((1,1))
+
+  with col1:
+    df_agg = df_.groupby(['Date','Assignee', 'Workspace Name']).agg({'Amount':sum})
+    g = df_agg['Amount'].groupby('Date', group_keys=False)
+    res = g.apply(lambda x: x.sort_values(ascending=False).head(5))
+    st.write(res)
+#     fig = px.histogram(df, x = 'Month', y='Amount', color="Squad", template = 'seaborn', barmode='group')
+#     fig.update_layout(title_text="MoM Expense by Squad",
+#                       yaxis_title="Amount", xaxis_title="Month")
+#     col1.plotly_chart(fig, use_container_width=True) 
+
+
+  with col2:
+    # copy data for the pie chart    
+#     df_=df[['Squad', 'Amount']].copy()
+#     data=df_.groupby(['Squad']).sum()['Amount']
+#     labels = df_['Squad'].unique().tolist()
+
+#     fig = px.pie(df_, values=data, names=labels, template = 'seaborn')
+
+#     fig.update_layout(title_text="Expense Breakdown by Squad")
+#     col2.plotly_chart(fig, use_container_width=True)  
+
+
+#   # display raw data
+#   fig = go.Figure(data=[go.Table(
+#       header=dict(values=list(df.columns),
+#                   fill_color='#264653',
+#                   font_color="white",
+#                   align='left'),
+#       cells=dict(values=[df.Squad, df.Category, df.Detail, df.Month, df.Amount],
+#                  fill_color='mintcream',
+#                  font_color="black",
+#                  align='left'))
+#   ])
+
+#   fig.update_layout(title_text="Raw Expense Data",title_font_color = '#264653',title_x=0,margin= dict(l=0,r=10,b=10,t=30), height=400)                                                               
+#   st.plotly_chart(fig, use_container_width=True)      
+
   
 with st.sidebar:
 #     st.write("Choose the time period")
