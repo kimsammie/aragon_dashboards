c4b6e68fcdcfbd3e974785cb681c0a7ecea11b01
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-24 11:45:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -486,6 +486,7 @@ with tab7:
   
   df_['Date']  = pd.to_datetime(df_['Date']).apply(lambda x: x.strftime('%Y-%m')) 
   df_.rename(columns={"Workspace Name": "Workspace_Name", "Task Name": "Task_Name"})
+  st.write(list(df_.columns))
 
   col1, col2 = st.columns((1,1))
 
