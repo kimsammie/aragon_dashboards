954919564e0c9310a38b2c6527ba5cfb6ac6d97e
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-02-28 15:30:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -808,6 +808,7 @@ lda_model = gensim.models.ldamodel.LdaModel(
 pprint(lda_model.print_topics())  # The trained topics (keywords and weights)
 
 def format_topics_sentences(ldamodel=None, corpus=corpus, texts=data):
+# Init output
 	sent_topics_df = pd.DataFrame()
 
 	# Get main topic in each document
@@ -818,26 +819,26 @@ def format_topics_sentences(ldamodel=None, corpus=corpus, texts=data):
 	    # Get the Dominant topic, Perc Contribution and Keywords for each document
 	    for j, (topic_num, prop_topic) in enumerate(row):
 		if j == 0:  # => dominant topic
-			wp = ldamodel.show_topic(topic_num)
-			topic_keywords = ", ".join([word for word, prop in wp])
-			sent_topics_df = sent_topics_df.append(
-				pd.Series(
-				    [int(topic_num), round(prop_topic, 4), topic_keywords]
-				),
-				ignore_index=True,
-			    )
-	     else:
-	    	break
-        sent_topics_df.columns = [
-            "Dominant_Topic",
-            "Perc_Contribution",
-            "Topic_Keywords",
-        ]
+		    wp = ldamodel.show_topic(topic_num)
+		    topic_keywords = ", ".join([word for word, prop in wp])
+		    sent_topics_df = sent_topics_df.append(
+			pd.Series(
+			    [int(topic_num), round(prop_topic, 4), topic_keywords]
+			),
+			ignore_index=True,
+		    )
+		else:
+		    break
+	sent_topics_df.columns = [
+	    "Dominant_Topic",
+	    "Perc_Contribution",
+	    "Topic_Keywords",
+	]
 
-        # Add original text to the end of the output
-        contents = pd.Series(texts)
-        sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
-        return sent_topics_df
+	# Add original text to the end of the output
+	contents = pd.Series(texts)
+	sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+	return sent_topics_df
 
 df_topic_sents_keywords = format_topics_sentences(
 ldamodel=lda_model, corpus=corpus, texts=data_ready
