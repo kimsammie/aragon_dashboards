d5263b2bc450840bfee5e73cb69b27c97a7361cc
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-03-01 14:57:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -747,17 +747,17 @@ with tab8:
 
 	# Tokenize Sentences and Clean
   def sent_to_words(sentences):
-	for sent in sentences:
-	    sent = re.sub("\S*@\S*\s?", "", sent)  # remove emails
-	    sent = re.sub("\s+", " ", sent)  # remove newline chars
-	    sent = re.sub("'", "", sent)  # remove single quotes
-	    sent = gensim.utils.simple_preprocess(
-		str(sent), deacc=True
-	    )  # split the sentence into a list of words. deacc=True option removes punctuations
-	    sent = [lemmatizer.lemmatize(w) for w in sent]
-	    yield (sent)
+        for sent in sentences:
+            sent = re.sub("\S*@\S*\s?", "", sent)  # remove emails
+            sent = re.sub("\s+", " ", sent)  # remove newline chars
+            sent = re.sub("'", "", sent)  # remove single quotes
+            sent = gensim.utils.simple_preprocess(
+                str(sent), deacc=True
+            )  # split the sentence into a list of words. deacc=True option removes punctuations
+            sent = [lemmatizer.lemmatize(w) for w in sent]
+            yield (sent)
 
-  # Convert to list
+    # Convert to list
   data = df_1wk.content.values.tolist()
   data_words = list(sent_to_words(data))
 
