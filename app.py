32562d40e297f9efbc570f94128951b0097428ee
kimsammie <65345731+kimsammie@users.noreply.github.com>
2023-03-01 15:05:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -861,27 +861,28 @@ with tab8:
   sent_topics_outdf_grpd = df_topic_sents_keywords.groupby("Dominant_Topic")
 
   for i, grp in sent_topics_outdf_grpd:
-	sent_topics_sorteddf_mallet = pd.concat(
-	    [
-		sent_topics_sorteddf_mallet,
-		grp.sort_values(["Perc_Contribution"], ascending=False).head(1),
-	    ],
-	    axis=0,
-	)
+        sent_topics_sorteddf_mallet = pd.concat(
+            [
+                sent_topics_sorteddf_mallet,
+                grp.sort_values(["Perc_Contribution"], ascending=False).head(1),
+            ],
+            axis=0,
+        )
 
-  # Reset Index
+    # Reset Index
   sent_topics_sorteddf_mallet.reset_index(drop=True, inplace=True)
 
-  # Format
+    # Format
   sent_topics_sorteddf_mallet.columns = [
-	"Topic_Num",
-	"Topic_Perc_Contrib",
-	"Keywords",
-	"Representative Text",
-	]
+        "Topic_Num",
+        "Topic_Perc_Contrib",
+        "Keywords",
+        "Representative Text",
+    ]
 
-  # Show
+    # Show
   sent_topics_sorteddf_mallet.head(10)
+
 
   # a word cloud with the size of the words proportional to the weight
   # 1. Wordcloud of Top N words in each topic
